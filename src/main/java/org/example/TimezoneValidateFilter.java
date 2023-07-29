204e659e0fe81ce46e8622f53834d1f59ac7f49b
Yareishe <121358447+Yareishe@users.noreply.github.com>
2023-07-29 21:07:03 +0300

    Update TimezoneValidateFilter.java

--- src/main/java/org/example/TimezoneValidateFilter.java
+++ src/main/java/org/example/TimezoneValidateFilter.java
@@ -1,38 +1,38 @@
+
 package org.example;
-import java.io.IOException;
-import javax.servlet.Filter;
+
+
+
+
+
 import javax.servlet.FilterChain;
-import javax.servlet.FilterConfig;
 import javax.servlet.ServletException;
-import javax.servlet.ServletRequest;
-import javax.servlet.ServletResponse;
 import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.time.zone.ZoneRulesException;
+import java.util.logging.Filter;
+import java.util.logging.LogRecord;
 
 @WebFilter("/time")
-public class TimezoneValidateFilter implements Filter {
-
+public class TimezoneValidateFilter extends HttpFilter {
     @Override
-    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
-            throws IOException, ServletException {
-        String timezone = request.getParameter("timezone");
-
-
-        if (timezone != null && !timezone.isEmpty()) {
-            if (isValidTimezone(timezone)) {
-                chain.doFilter(request, response);
-            } else {
-
-                response.setContentType("text/html");
-                response.setCharacterEncoding("UTF-8");
-                response.getWriter().write("Invalid timezone");
-            }
+    protected void doFilter(HttpServletRequest req,
+                            HttpServletResponse resp,
+                            FilterChain chain) throws IOException, ServletException {
+        String timezone = req.getParameter("timezone");
+        if (timezone == null || timezone.isEmpty()) {
+            chain.doFilter(req, resp);
         } else {
-            chain.doFilter(request, response);
+            try {
+                chain.doFilter(req, resp);
+            } catch (ZoneRulesException e) {
+                resp.setStatus(400);
+                resp.getWriter().write("Invalid timezone!");
+            }
         }
     }
-
-
-    private boolean isValidTimezone(String timezone) {
-        return !timezone.toLowerCase().contains("maxim");
-    }
 }
+
