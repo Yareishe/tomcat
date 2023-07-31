c894be8c3a914fa286ae7519f7a6824e32b21fb8
Yareishe <121358447+Yareishe@users.noreply.github.com>
2023-08-01 00:15:55 +0300

    Update TimeServlet.java

--- src/main/java/org/example/TimeServlet.java
+++ src/main/java/org/example/TimeServlet.java
@@ -1,35 +1,89 @@
+
 package org.example;
 
 
-
-
 import javax.servlet.annotation.WebFilter;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.net.URLDecoder;
 import java.net.URLEncoder;
 import java.nio.charset.StandardCharsets;
 import java.time.LocalDateTime;
 import java.time.ZoneId;
 import java.time.format.DateTimeFormatter;
-
+import java.util.Date;
 
 public class TimeServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        if (req.getParameter("timezone") == null || req.getParameter("timezone").isEmpty()) {
+            resp.getWriter().write(getDate("UTC"));
 
-            resp.getWriter()
-                    .write(DateTimeFormatter.ISO_LOCAL_TIME
-                            .format(LocalDateTime.now(ZoneId.of(
-                                            URLEncoder.encode(
-                                                    req.getParameter("timezone"), String.valueOf(StandardCharsets.UTF_8)
-                                            )
-                                    ))
-                            )
-                    );
+        } else {
+            String timezone = req.getParameter("timezone");
+            resp.getWriter().write(getDate(timezone));
+        }
+        resp.getWriter().close();
+    }
+    public static String getDate(String param) throws UnsupportedEncodingException {
+        return DateTimeFormatter.ISO_LOCAL_TIME
+                .format(LocalDateTime.now(ZoneId.of(
+                        URLDecoder.decode(
+                                param, String.valueOf(StandardCharsets.UTF_8)
+                        ))));
+    }
+
+
+    public static String getDate1(String param) {
+        Date actualDate = new Date();
+        DateTimeFormatter dateFormat = DateTimeFormatter.ofPattern("uuuu-MM-dd HH:mm:ss z")
+                .withZone(ZoneId.of(param));
+        return dateFormat.format(actualDate.toInstant());
+    }
+
+}
+
+/*
+
+
+ if (req.getParameter("timezone") == null || req.getParameter("timezone").isEmpty()) {
+            resp.getWriter().write(getDate("UTC"));
+
+        } else {
+            String timezone = req.getParameter("timezone");
+            String date = getDate(timezone);
+            resp.getWriter().write(date);
+        }
 
         resp.getWriter().close();
     }
-}
+
+    public static String getDate(String param) {
+        Date actualDate = new Date();
+        DateTimeFormatter dateFormat = DateTimeFormatter.ofPattern("uuuu-MM-dd HH:mm:ss z")
+                .withZone(ZoneId.of(param));
+        return dateFormat.format(actualDate.toInstant());
+
+
+        resp.getWriter()
+                .write(DateTimeFormatter.ISO_LOCAL_TIME
+                        .format(LocalDateTime
+                                .now(ZoneId.of
+                                        (URLDecoder.decode(req.getParameter("timezone"), String.valueOf(StandardCharsets.UTF_8)
+                                        )))));
+
+        String timezone = req.getParameter("timezone");
+        String timezoneDecode = URLDecoder.decode(timezone,String.valueOf(StandardCharsets.UTF_8));
+        ZoneId timezoneid = ZoneId.of(timezoneDecode);
+        LocalDateTime timezoneLocal = LocalDateTime.now(timezoneid);
+        DateTimeFormatter timezoneformat = DateTimeFormatter.ISO_LOCAL_TIME;
+        String timezoneformatTime = timezoneformat.format(timezoneLocal);
+        resp.getWriter().write(timezoneformatTime);
+        System.out.println(timezoneid);
+
+         */
